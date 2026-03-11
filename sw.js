2b20cc7fc5f136b6fe1ddcde6241eeca2b20a7f9
HazeV98 <mattia.bacci.mb@gmail.com>
2026-03-11 23:06:44 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
-. 
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installato');
+});
+
+self.addEventListener('fetch', (event) => {
+  // Lasciamo passare le richieste normalmente
+  event.respondWith(fetch(event.request));
+});

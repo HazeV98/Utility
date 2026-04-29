63415fb7f91783fb1028f3728c6f5dc730ba432f
HazeV98 <mattia.bacci.mb@gmail.com>
2026-04-29 14:57:59 +0200

    Update Firebase SDK version in service worker

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,27 +1,15 @@
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js');
 
-// La tua configurazione Firebase
-const firebaseConfig = {
+firebase.initializeApp({
     apiKey: "AIzaSyDpamGt2bsT6TJMwnerIUTSfCVFBTJtos4",
     authDomain: "utility-haze.firebaseapp.com",
     projectId: "utility-haze",
-    storageBucket: "utility-haze.firebasestorage.app",
+    storageBucket: "utility-haze.appspot.com",
     messagingSenderId: "686237947418",
     appId: "1:686237947418:web:f03ba19ab8fff43110a3a3"
-};
-
-firebase.initializeApp(firebaseConfig);
-const messaging = firebase.messaging();
-
-// Gestione delle notifiche quando l'app è in background o chiusa
-messaging.onBackgroundMessage((payload) => {
-    console.log('[firebase-messaging-sw.js] Ricevuto messaggio in background:', payload);
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-        icon: 'icon-512.png' // L'icona che apparirà nella notifica (assicurati che il nome sia corretto)
-    };
-    self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
+// Inizializza il modulo: ricevendo payload con "notification", 
+// Firebase mostrerà automaticamente un singolo banner di sistema.
+const messaging = firebase.messaging();

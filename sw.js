0531d6701819d1342d3a81378e5a7b5f5ecd7135
HazeV98 <mattia.bacci.mb@gmail.com>
2026-09-23 03:57:36 +0200

    Update CACHE_NAME to utility-app-v29

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'utility-app-v28';
+const CACHE_NAME = 'utility-app-v29';
 
 // Elenco completo dei file dell'app basato su mappa_file.json
 const PRECACHE_ASSETS = [
@@ -10,6 +10,7 @@ const PRECACHE_ASSETS = [
     './moduli/admin.js',
     './moduli/auth.js',
     './moduli/bacheca.js',
+    './moduli/navigatore.js',
     './moduli/bacheca_turni.js',
     './moduli/bacheca_utility.js',
     './moduli/bateolive.js',
